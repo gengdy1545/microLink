67857379ec03b7599389b5143ae34ce00153557c
dongyang geng <gengdy.ruc@gmail.com>
2025-12-27 16:12:50 +0800

    fix

--- microlink-workflow/src/main/java/org/microserviceteam/workflow/delegate/content/ContentRejectDelegate.java
+++ microlink-workflow/src/main/java/org/microserviceteam/workflow/delegate/content/ContentRejectDelegate.java
@@ -2,15 +2,31 @@ package org.microserviceteam.workflow.delegate.content;
 
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
+import org.microserviceteam.workflow.client.ContentClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("contentRejectDelegate")
 public class ContentRejectDelegate implements JavaDelegate {
 
+    @Autowired
+    private ContentClient contentClient;
+
     @Override
     public void execute(DelegateExecution execution) {
-        Object contentId = execution.getVariable("contentId");
+        Object contentIdObj = execution.getVariable("contentId");
+        if (contentIdObj == null) {
+            System.err.println("Content ID not found in process variables");
+            return;
+        }
+
+        Long contentId = Long.valueOf(contentIdObj.toString());
         System.out.println("Content rejected: " + contentId);
-        // 这里可以更新内容状态为 REJECTED
+        
+        try {
+            contentClient.updateStatus(contentId, "REJECTED");
+        } catch (Exception e) {
+            System.err.println("Feign call to content service failed for rejection: " + e.getMessage());
+        }
     }
 }
